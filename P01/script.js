1e1644b8fc0ff5b8c71ce2cc2a8c7665f07917e6
mariamfaizan2020 <87652667+mariamfaizan2020@users.noreply.github.com>
2021-08-09 19:36:22 +0500

    updated svcript.js
    added email validation with simple javascript

--- P01/script.js
+++ P01/script.js
@@ -16,27 +16,42 @@ function showError(input,message){
 function showSuccess(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+}
+
+//function to check if email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 
 }
+
+
 //This is an eventListener for the form on submit
 form.addEventListener('submit',function(e){
     e.preventDefault();
    
     if ( username.value==='') {
         showError(username,'Username is required')
-   } else {
+   }  else {
        showSuccess(username);
    }
+
    if ( email.value==='') {
     showError(email,'email is required')
-} else {
+ }  else if (!isValidEmail(email.value)) {
+    showError(email,'Email is invalid')
+}
+ else {
    showSuccess(email);
-} if ( password.value==='') {
+} 
+
+    if ( password.value==='') {
     showError(password,'password is required')
 } else {
    showSuccess(password);
 }
-if ( password2.value==='') {
+
+    if ( password2.value==='') {
     showError(password2,'confirm password is required')
 } else {
    showSuccess(password2);
