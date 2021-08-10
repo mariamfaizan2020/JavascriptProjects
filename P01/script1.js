c7f4df81fec3f54903ddec7589b98f91c6cc66ac
mariamfaizan2020 <87652667+mariamfaizan2020@users.noreply.github.com>
2021-08-10 16:56:29 +0500

    archived script.js1 and created new script.js
    refactored required field check

--- P01/script1.js
+++ P01/script1.js
@@ -3,7 +3,6 @@ const username=document.getElementById('username');
 const email=document.getElementById('email');
 const password=document.getElementById('password');
 const password2=document.getElementById('password2');
-
 //All functions
 // functions to show error
 function showError(input,message){
@@ -12,37 +11,51 @@ function showError(input,message){
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
+
 //funtion to show success
 function showSuccess(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
+
 //function to check if email is valid
 function isValidEmail(email){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 
 }
-//function to check if required fields have data
-function chekcRequired(inputArray){
-     inputArray.forEach(function(input){
-      if (input.value==='')   {
-         showError(input,`${getFieldId(input)} is required`)
-      } else {
-          showSuccess(input);
-      }
-    
-     });
-}
-//function to get Id of the input field with correct case format.
-function getFieldId(input){
-    return input.id.charAt(0).toUpperCase()+ input.id.slice(1)
-}
+
+
 //This is an eventListener for the form on submit
 form.addEventListener('submit',function(e){
     e.preventDefault();
    
-   chekcRequired([username,email,password,password2]) 
+    if ( username.value==='') {
+        showError(username,'Username is required')
+   }  else {
+       showSuccess(username);
+   }
+
+   if ( email.value==='') {
+    showError(email,'email is required')
+ }  else if (!isValidEmail(email.value)) {
+    showError(email,'Email is invalid')
+}
+ else {
+   showSuccess(email);
+} 
+
+    if ( password.value==='') {
+    showError(password,'password is required')
+} else {
+   showSuccess(password);
+}
+
+    if ( password2.value==='') {
+    showError(password2,'confirm password is required')
+} else {
+   showSuccess(password2);
+}
 
 
 })
